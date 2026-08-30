4a81005c2bbc92c86b742b1971223918882fcdbb
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-30 08:35:48 -0300

    refactor(api): route backtest catalog through injected use case

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import type { BacktestCatalogUseCase } from "../application/backtestCatalog.js";
 import type { StrategyCatalogUseCase } from "../application/strategyCatalog.js";
 import { serveDataStatus } from "./dataStatus.js";
 import { serveStrategyLab } from "./strategyLab.js";
@@ -8,11 +9,13 @@ import { serveGameComparison } from "./gameComparison.js";
 import { serveAiInsights } from "./aiInsights.js";
 import { serveOperations } from "./operations.js";
 import { serveAgenda } from "./agenda.js";
+import { serveBacktests } from "./backtests.js";
 import { serveStrategies } from "./strategies.js";
 import { serveAnalysisJobs } from "./analysisJobs.js";
 import type { LotoLabServerOptions } from "./server.js";
 
 export interface FeatureRouteDependencies {
+  backtestCatalog: BacktestCatalogUseCase;
   strategyCatalog: StrategyCatalogUseCase;
 }
 
@@ -25,6 +28,8 @@ type FeatureRouteHandler = (
 
 const featureRoutes: FeatureRouteHandler[] = [
   serveDataStatus,
+  (request, response, options, dependencies) =>
+    serveBacktests(request, response, options, dependencies.backtestCatalog),
   (request, response, options, dependencies) =>
     serveStrategies(request, response, options, dependencies.strategyCatalog),
   serveAnalysisJobs,
