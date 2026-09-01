d19f7e0edf769ba599a961134619e0b2f892c570
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-09-01 07:08:34 -0300

    refactor: remove facade concreta da API

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,8 +1,6 @@
-import type { Pool } from "pg";
 import type { AdvancedAnalysis } from "../analysis/advancedTypes.js";
 import type { AdvancedAnalysisResponse } from "../application/analyzeAdvancedLottery.js";
 import type { AnalysisResponse } from "../application/analyzeLottery.js";
-import { CheckGameBatchUseCase, type CheckGameBatchResult } from "../application/checkGameBatch.js";
 import {
   InsufficientGenerationHistoryError,
   MIN_GENERATION_HISTORY,
@@ -12,15 +10,9 @@ import {
 import {
   BacktestRoundLimitError,
   MAX_BACKTEST_ROUNDS,
-  RunBacktestUseCase,
   type RunBacktestRequest,
   type RunBacktestResponse,
 } from "../application/runBacktest.js";
-import { PostgresContestRepository } from "../persistence/contestRepository.js";
-import { PostgresGameRepository } from "../persistence/gameRepository.js";
-import { PostgresStrategyRepository } from "../persistence/strategyRepository.js";
-import { PostgresBacktestRepository } from "../persistence/backtestRepository.js";
-import type { StrategyRecord, UpsertStrategyInput } from "../persistence/types.js";
 
 export {
   BacktestRoundLimitError,
@@ -37,33 +29,3 @@ export type {
   RunBacktestRequest,
   RunBacktestResponse,
 };
-
-export class LotoLabApiServices {
-  readonly contests: PostgresContestRepository;
-  readonly games: PostgresGameRepository;
-  readonly strategies: PostgresStrategyRepository;
-  readonly backtests: PostgresBacktestRepository;
-  private readonly checkGameBatch: CheckGameBatchUseCase;
-  private readonly runBacktestUseCase: RunBacktestUseCase;
-
-  constructor(pool: Pool) {
-    this.contests = new PostgresContestRepository(pool);
-    this.games = new PostgresGameRepository(pool);
-    this.strategies = new PostgresStrategyRepository(pool);
-    this.backtests = new PostgresBacktestRepository(pool);
-    this.checkGameBatch = new CheckGameBatchUseCase(this.games, this.contests);
-    this.runBacktestUseCase = new RunBacktestUseCase(this.contests, this.backtests);
-  }
-
-  async checkBatch(batchId: number, contestNumber: number): Promise<CheckGameBatchResult> {
-    return this.checkGameBatch.execute(batchId, contestNumber);
-  }
-
-  async runBacktest(input: RunBacktestRequest): Promise<RunBacktestResponse> {
-    return this.runBacktestUseCase.execute(input);
-  }
-
-  async upsertStrategy(input: UpsertStrategyInput): Promise<StrategyRecord> {
-    return this.strategies.upsert(input);
-  }
-}
